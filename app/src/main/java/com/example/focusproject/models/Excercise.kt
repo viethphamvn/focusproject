4ed677c1e1509b07bdefb232449cb5e1ba00c11b
Viet Pham <tug36669@temple.edu>
2020-04-27 18:44:11 -0400

    Drew routine and resttime item + Added more attribute for Exercise class

--- app/src/main/java/com/example/focusproject/models/Excercise.kt
+++ app/src/main/java/com/example/focusproject/models/Excercise.kt
@@ -4,14 +4,22 @@ import com.bumptech.glide.Glide.init
 import java.lang.reflect.Array.get
 import java.lang.reflect.Array.set
 
-class Excercise(name: String, code: String, img: String){
+class Excercise(name: String, code: String, setnrep:HashMap<Int, Int>, weight:HashMap<Int, Int>, duration: Int,  img: String, isRestTime: Boolean){
     var name: String
     var code: String
     var img: String
+    var isRestTime: Boolean
+    var setnrep: HashMap<Int, Int>
+    var weight: HashMap<Int, Int>
+    var duration: Int
 
     init{
         this.name = name
         this.code = code
         this.img = img
+        this.isRestTime = isRestTime
+        this.setnrep = HashMap<Int, Int>(setnrep)
+        this.weight = HashMap(weight)
+        this.duration = duration
     }
 }
