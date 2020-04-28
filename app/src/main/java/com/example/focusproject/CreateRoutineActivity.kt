266f92d13f2886e00ac52e899293c34df7bf0c3c
Viet Pham <tug36669@temple.edu>
2020-04-28 13:26:57 -0400

    Added New Activities

---
+++ app/src/main/java/com/example/focusproject/CreateRoutineActivity.kt
@@ -0,0 +1,12 @@
+package com.example.focusproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateRoutineActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_routine)
+    }
+}
