a2efd93a1b5df5d96566d8cb2101e912d1583884
Viet Pham <tug36669@temple.edu>
2020-05-21 12:09:47 -0400

    Implemented check and functionality for NewWorkoutEditFragment. Code Cleanup. Extract String Resources

--- app/src/main/java/com/example/focusproject/fragments/NewWorkoutEditFragment.kt
+++ app/src/main/java/com/example/focusproject/fragments/NewWorkoutEditFragment.kt
@@ -1,24 +1,27 @@
 package com.example.focusproject.fragments
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
+import androidx.core.view.isGone
 import com.example.focusproject.CreateRoutineActivity
 
 import com.example.focusproject.R
 import com.example.focusproject.RoutineEditActivity
 import com.example.focusproject.models.Exercise
+import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.ikovac.timepickerwithseconds.MyTimePickerDialog
 import kotlinx.android.synthetic.main.fragment_new_workout_edit.view.*
 import kotlinx.android.synthetic.main.fragment_new_workout_edit.view.isRestTimeCheckBox
+import org.w3c.dom.Text
+import java.time.Duration
 
 class NewWorkoutEditFragment : Fragment() {
     var hours : Int = 0
@@ -35,7 +38,7 @@ class NewWorkoutEditFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         fragmentView = inflater.inflate(R.layout.fragment_new_workout_edit, container, false)
-        var currentDurationTextView = fragmentView.currentDurationTextView
+        val currentDurationTextView = fragmentView.currentDurationTextView
 
         setUpSpinner()
 
@@ -49,67 +52,189 @@ class NewWorkoutEditFragment : Fragment() {
                         minutes = minute
                         this.seconds = seconds
                     }
-                }, 0, 0, 45, true).show()
+                }, 0, 0, 0, true).show()
         }
 
         fragmentView.apply {
             saveButton.setOnClickListener {
-                var isRestTime = isRestTimeCheckBox.isChecked
-                var exerciseName = workoutNameEditText.editText?.text.toString()
-                var equipmentNeeded = needEquipmentCheckBox.isChecked
-                var exerciseDesc = workoutDescEditText.editText?.text.toString()
-                var type = spinnerExerciseType.selectedItem.toString()
-                var gifUrl = workoutImgUrlEditText.editText?.text.toString()
-                var youtubeUrl = workoutVideoUrlEditText.editText?.text.toString()
-                var rep = repTextView.text.toString().toLong()
-                var weight = weightTextView.text.toString().toLong()
-                var duration = (hours * 3600 + minutes * 60 + seconds).toLong()
+                val isRestTime = isRestTimeCheckBox.isChecked
+                val exerciseName = workoutNameEditText.editText?.text.toString()
+                val equipmentNeeded = needEquipmentCheckBox.isChecked
+                val exerciseDesc = workoutDescEditText.editText?.text.toString()
+                val type = spinnerExerciseType.selectedItem.toString()
+                val gifUrl = workoutImgUrlEditText.editText?.text.toString()
+                val youtubeUrl = workoutVideoUrlEditText.editText?.text.toString()
+                val rep = repTextView.text.toString().toLong()
+                val weight = weightTextView.text.toString().toLong()
+                val duration = (hours * 3600 + minutes * 60 + seconds).toLong()
 
-                var firestore = FirebaseFirestore.getInstance()
-                var ref = firestore.collection("Exercise").document()
+                if (validateInfo(isRestTime, exerciseName, type, gifUrl, youtubeUrl, duration)) { //Check information before submit
+                    val firestore = FirebaseFirestore.getInstance()
+                    val ref = firestore.collection("Exercise").document()
 
-                var exercise : HashMap<String, Any> = HashMap()
-                exercise.put("isRestTime", isRestTime)
-                exercise.put("name", exerciseName)
-                exercise.put("equipmentNeeded", equipmentNeeded)
-                exercise.put("desc", exerciseDesc)
-                exercise.put("type", type)
-                exercise.put("img", gifUrl)
-                exercise.put("vidId", youtubeUrl)
-                exercise.put("rep", rep)
-                exercise.put("isTimed",duration > 0)
-                exercise.put("weight", weight)
-                exercise.put("duration", duration)
-                exercise.put("createdBy", FirebaseAuth.getInstance().currentUser?.uid!!)
-                exercise.put("uid",ref.id)
+                    val exercise: HashMap<String, Any> = HashMap()
+                    exercise["isRestTime"] = isRestTime
+                    exercise["name"] = exerciseName
+                    exercise["equipmentNeeded"] = equipmentNeeded
+                    exercise["desc"] = exerciseDesc
+                    exercise["type"] = type
+                    exercise["img"] = gifUrl
+                    exercise["vidId"] = youtubeUrl
+                    exercise["rep"] = rep
+                    exercise["isTimed"] = duration > 0
+                    exercise["weight"] = weight
+                    exercise["duration"] = duration
+                    exercise["createdBy"] = FirebaseAuth.getInstance().currentUser?.uid!!
+                    exercise["uid"] = ref.id
 
-                ref.set(exercise).addOnSuccessListener {
-                    Toast.makeText(context, "Exercise Created", Toast.LENGTH_SHORT).show()
-                }.addOnFailureListener{
+                    ref.set(exercise).addOnSuccessListener {
+                        Toast.makeText(context, "Exercise Created", Toast.LENGTH_SHORT).show()
+                        exerciseCreatedCallBack(exercise)
+                    }.addOnFailureListener {
+                        Toast.makeText(context, "Error Occured", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(context, "Info missing", Toast.LENGTH_SHORT).show()
+                    //Handle error
+                }
+            }
+
+            isRestTimeCheckBox.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked){
+                    fragmentView.workoutDescEditText.visibility = View.GONE
+                    fragmentView.workoutNameEditText.visibility = View.GONE
+                    fragmentView.needEquipmentCheckBox.visibility = View.GONE
+                    fragmentView.needEquipmentTextView.visibility = View.GONE
+                    fragmentView.spinnerExerciseType.visibility = View.GONE
+                    fragmentView.repTextView.visibility = View.GONE
+                    fragmentView.decreaseRepButton.visibility = View.GONE
+                    fragmentView.increaseRepButton.visibility = View.GONE
+                    fragmentView.increaseWeightButton.visibility = View.GONE
+                    fragmentView.decreaseWeightButton.visibility = View.GONE
+                    fragmentView.weightTextView.visibility = View.GONE
+                } else {
+                    fragmentView.workoutDescEditText.visibility = View.VISIBLE
+                    fragmentView.workoutNameEditText.visibility = View.VISIBLE
+                    fragmentView.needEquipmentCheckBox.visibility = View.VISIBLE
+                    fragmentView.needEquipmentTextView.visibility = View.VISIBLE
+                    fragmentView.spinnerExerciseType.visibility = View.VISIBLE
+                    fragmentView.repTextView.visibility = View.VISIBLE
+                    fragmentView.decreaseRepButton.visibility = View.VISIBLE
+                    fragmentView.increaseRepButton.visibility = View.VISIBLE
+                    fragmentView.increaseWeightButton.visibility = View.VISIBLE
+                    fragmentView.decreaseWeightButton.visibility = View.VISIBLE
+                    fragmentView.weightTextView.visibility = View.VISIBLE
+                }
+            }
+
+            imageInput.addTextChangedListener(object: TextWatcher{
+                override fun afterTextChanged(s: Editable?) {
+                    workoutVideoUrlEditText.isEnabled = s.toString() == ""
+                }
+
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
 
                 }
-                exerciseCreatedCallBack(exercise)
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                }
+
+            })
+
+            videoInput.addTextChangedListener(object: TextWatcher{
+                override fun afterTextChanged(s: Editable?) {
+                    workoutImgUrlEditText.isEnabled = s.toString() == ""
+                }
+
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                }
+
+            })
+
+            increaseRepButton.setOnClickListener {
+                if ((repTextView as TextView).text.toString() != "") {
+                    (repTextView as TextView).text = ((repTextView as TextView).text.toString().toLong() + 1).toString()
+                } else {
+                    (repTextView as TextView).text = "0"
+                }
+            }
+
+            decreaseRepButton.setOnClickListener {
+                if ((repTextView as TextView).text.toString() != "" && (repTextView as TextView).text.toString().toLong() > 0) {
+                    (repTextView as TextView).text = ((repTextView as TextView).text.toString().toLong() - 1).toString()
+                } else {
+                    (repTextView as TextView).text = "0"
+                }
+            }
+
+            increaseWeightButton.setOnClickListener {
+                if ((weightTextView as TextView).text.toString() != "") {
+                    (weightTextView as TextView).text = ((weightTextView as TextView).text.toString().toLong() + 1).toString()
+                } else {
+                    (weightTextView as TextView).text = "0"
+                }
+            }
+
+            decreaseWeightButton.setOnClickListener {
+                if ((weightTextView as TextView).text.toString() != "" && (weightTextView as TextView).text.toString().toLong() > 0) {
+                    (weightTextView as TextView).text = ((weightTextView as TextView).text.toString().toLong() - 1).toString()
+                } else {
+                    (weightTextView as TextView).text = "0"
+                }
             }
         }
 
         return fragmentView
     }
 
+    private fun validateInfo(isRestTime: Boolean, name: String, type: String, img: String, vid: String, duration: Long): Boolean {
+        if (isRestTime){
+            if (img != "" || vid != ""){
+                if (duration > 0L){
+                    return true
+                }
+            }
+        } else {
+            if (name != "" && type != getString(R.string.typeNotSet)){
+                if (img != "" || vid != ""){
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
     private fun exerciseCreatedCallBack(exercise: HashMap<String, Any>){
-        var newExercise = Exercise(
-            exercise.get("name") as String,
-            exercise.get("type") as String,
-            exercise.get("uid") as String,
-            exercise.get("duration") as Long,
-            exercise.get("img") as String,
-            exercise.get("vidId") as String,
-            exercise.get("isRestTime") as Boolean,
-            exercise.get("isTimed") as Boolean,
-            exercise.get("rep") as Long,
-            exercise.get("equipmentNeeded") as Boolean,
-            exercise.get("weight") as Long,
-            exercise.get("createdBy") as String,
-            exercise.get("desc") as String)
+        val newExercise = Exercise(
+            exercise["name"] as String,
+            exercise["type"] as String,
+            exercise["uid"] as String,
+            exercise["duration"] as Long,
+            exercise["img"] as String,
+            exercise["vidId"] as String,
+            exercise["isRestTime"] as Boolean,
+            exercise["isTimed"] as Boolean,
+            exercise["rep"] as Long,
+            exercise["equipmentNeeded"] as Boolean,
+            exercise["weight"] as Long,
+            exercise["createdBy"] as String,
+            exercise["desc"] as String)
 
         if (activity is RoutineEditActivity){
             (activity as RoutineEditActivity).onItemClick(newExercise)
@@ -124,7 +249,7 @@ class NewWorkoutEditFragment : Fragment() {
         typeSpinner.adapter =
             context?.let { ArrayAdapter<String>(it, R.layout.center_textview_for_spinner,resources.getStringArray(R.array.type_array)) }
         //Set Up Default Option
-        var defaultPosition = 0
+        val defaultPosition = 0
 
         typeSpinner.setSelection(defaultPosition)
         //TODO Center the options https://stackoverflow.com/questions/7511049/set-view-text-align-at-center-in-spinner-in-android
